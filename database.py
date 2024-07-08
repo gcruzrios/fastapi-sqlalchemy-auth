5155b53cc279cab25661a7260a0a571193c46930
gcruzrios <gcruzr@gmail.com>
2024-07-07 19:05:25 -0600

    get users completed

--- database.py
+++ database.py
@@ -6,13 +6,13 @@ from sqlalchemy.orm import sessionmaker
 #SQLite
 
 #sqliteName = 'users.sqlite'
-#base_dir = os.path.dirname(os.path.realpath(__file__))
-#databaseUrl = f'sqlite:///{os.path.join(base_dir, sqliteName)}'
-#engine = create_engine(databaseUrl, echo =True)
+base_dir = os.path.dirname(os.path.realpath(__file__))
+databaseUrl = f'sqlite:///{os.path.join(base_dir, sqliteName)}'
+engine = create_engine(databaseUrl, echo =True)
 
 #Postgres
-url = "postgresql+psycopg2://postgres:Grvn240675$$@localhost:5432/authpg"
-engine = create_engine(url, echo=True)
+#url = "postgresql+psycopg2://postgres:Grvn240675$$@localhost:5432/authpg"
+#engine = create_engine(url, echo=True)
 
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
