4a31084dcf77f82d34f727c6ab1f49e06b562fbd
gcruzrios <gcruzr@gmail.com>
2024-07-07 18:09:47 -0600

    second commit

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-url = "postgresql+psycopg2://postgres:Grvn240675$$@localhost:5432/authpg"
-engine = create_engine(url, echo=True)
+#SQLite
+sqliteName = 'users.sqlite'
+base_dir = os.path.dirname(os.path.realpath(__file__))
+databaseUrl = f'sqlite:///{os.path.join(base_dir, sqliteName)}'
+engine = create_engine(databaseUrl, echo =True)
+
+#Postgres
+#url = "postgresql+psycopg2://postgres:Grvn240675$$@localhost:5432/authpg"
+#engine = create_engine(url, echo=True)
 
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from auth_bearer import JWTBearer
 from functools import wraps
 from utils import create_access_token,create_refresh_token,verify_password,get_hashed_password
 from auth_bearer import JWTBearer
-
+from decouple import config
 from routes.user import routerUser
-
-
+import os
+import uvicorn
 
 app = FastAPI(
     title="Auth FastAPI",
-    description="Api en primeros pasos",
+    description="Api AUTH User",
     version="0.0.1",
 )
 
@@ -29,7 +29,9 @@ def read_root():
     return {'message':'Hello world'}
 
 
-
+ALGORITHM = config("ALGORITHM")
+JWT_SECRET_KEY = config("JWT_SECRET_KEY")   # should be kept secret
+JWT_REFRESH_SECRET_KEY = config("JWT_REFRESH_SECRET_KEY")
 
 def token_required(func):
     @wraps(func)
@@ -45,3 +47,8 @@ def token_required(func):
             return {'msg': "Token blocked"}
         
     return wrapper
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run("main:app", host="0.0.0.0", port=port) 
+
